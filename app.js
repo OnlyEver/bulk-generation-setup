7fde6d96fbbebdf1fc34275fab31f0c1235a6cdb
rohel04 <rohelshk@gmail.com>
2025-01-08 10:11:30 +0545

    get results api

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { sendGeneration } from "./src/send_generation.js";
 import { checkBatchStatus } from "./src/check_batch_status.js";
+import { getResult } from "./src/get_result.js";
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -17,6 +18,12 @@ app.get("/send-generation", async (req, res) => {
 app.get("/check-status", async (req, res) => {
   // const data = await checkBatchStatus("1");
   const data = await checkBatchStatus('batch_677d070c405881909d5c6373fea1ab91');
+  // batch_677d070dfd008190ad9b8a48cf6717e4
+  res.send(data);
+});
+app.get("/get-results", async (req, res) => {
+  // const data = await checkBatchStatus("1");
+  const data = await getResult('batch_677d070c405881909d5c6373fea1ab91');
   // batch_677d070dfd008190ad9b8a48cf6717e4
   res.send(data);
 });

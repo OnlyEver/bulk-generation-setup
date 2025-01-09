9691e5ca3582553622d0603204f8a190357896b4
anishghale007 <anishghalegg7@gmail.com>
2025-01-09 11:19:09 +0545

    connection file updated

--- src/mongodb/connection.ts
+++ src/mongodb/connection.ts
@@ -1,7 +1,9 @@
 import { MongoClient } from "mongodb";
+import { config } from "../config";
 
-const dbName = "onlyever";
-const db_uri = "mongodb://localhost:27017";
+const dbName = config.dbName || "";
+// const db_uri = "mongodb://localhost:27017";
+const db_uri = config.dbUri || "mongodb://localhost:27017";
 const client = new MongoClient(db_uri);
 const database = client.db(dbName);
 export const sourceCollection = database.collection("_source");

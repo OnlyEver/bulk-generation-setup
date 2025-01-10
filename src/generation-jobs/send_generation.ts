a476486f42708b29a4d004dd088b6a4fbed927e1
anishghale007 <anishghalegg7@gmail.com>
2025-01-10 13:40:43 +0545

    card generation and typology prompts files added

--- src/generation-jobs/send_generation.ts
+++ src/generation-jobs/send_generation.ts
@@ -1,13 +1,3 @@
-import OpenAI from "openai";
-import fs from "fs";
-import fsPromise from "fs/promises";
-
-import { returnTypologyPrompt } from "./1.batch-prepare/fetch-prompts/typology_prompt";
-import { parseData } from "./1.batch-prepare/parse_source_content";
-import { config } from "../config";
-import { delay } from "../utils/delay_helper";
-
-import { cardCollection, sourceCollection, typologyCollection } from "../mongodb/connection";
 
 import { getResult } from "./4.batch-result/get_result";
 import { checkBatchStatus } from "./3.batch-status/check_batch_status";
@@ -20,16 +10,6 @@ import { Card, insertCard, insertSourceTypology } from "../mongodb/insert";
 import { prepareBatch } from "./1.batch-prepare/prepare_batch";
 import { createBatch } from "./2.batch-creation/create_batch";
 import { BatchStatusEnum } from "../enums/batch_status";
-import { prepareBatchForCard } from "./1.batch-prepare/prepare_card_batch";
-
-
-type BatchStatus = {
-  id: string;
-  status: string;
-  file?: string;
-};
-
-
 
 export async function sendGeneration() {
   const openai = new OpenAI({
@@ -51,6 +31,9 @@ export async function sendGeneration() {
     await sendCardGeneration(response, docs);
   } else {
     //handle failure
+    if (batchStatus.file) {
+      await handleBatchFailure(batchStatus.file!);
+    } 
   }
   const data = {
     generation: "Generation will be handled here",
@@ -86,64 +69,15 @@ async function poolBatchStatus(batchId: string): Promise<BatchStatus> {
     return {
       id: "sda",
       status: BatchStatusEnum.FAILED,
+      file: batchStatus.error_file_id,
     };
   }
 }
 
-
-
-export async function sendCardGeneration(response: any[], docs: WithId<BSON.Document>[]) {
-  const openai = new OpenAI({
-    apiKey: config.openAiKey,
-  });
-
-  const sourceId = await prepareBatchForCard(response, docs);
-  var batch = await createBatch('./batchinputForCardGen.jsonl');
-
-  const batchStatus = await poolBatchStatus(
-    batch.id
-  );
-  if (batchStatus.status == "completed") {
-    const generationContent = await getResult(batchStatus.file!);
-    console.log(generationContent);
-    generationContent.forEach(async (element) => {
-      const body = element['response']['body'];
-      const content = body.choices[0]['message']['content'];
-      const parsedContent = JSON.parse(content);
-
-      console.log('Logging content');
-      console.log(parsedContent['test_cards']);
-
-      const source = docs.find((doc) => doc._id == sourceId);
-      var headings = [];
-      if (source) {
-        if (source.headings) {
-          headings = source.headings;
-        }
-      }
-      console.log(source);
-      const parsedC = parseCardGenResponse(parsedContent, false, headings);
-      if (parsedC.cards_data != null) {
-        const cards: Card[] = Array.from(parsedC.cards_data!.entries()).map(([key, value]) => ({
-          id: new ObjectId(key), // Convert the key to ObjectId
-          heading: value.heading,
-          content: value.content,
-          concepts: value.concepts,
-          facts: value.facts,
-          bloomlevel: value.bloomLevel,
-          displayTitle: value.displayTitle,
-        }));
-        await Promise.all(
-          cards.map(async (card: Card) => {
-            await insertCard(card, sourceId);
-          }));
-      }
-    });
-
-  } else {
-    //handle failure
-  }
-
+type BatchStatus = {
+  id: string;
+  status: BatchStatusEnum;
+  file?: string;
 };
 
 

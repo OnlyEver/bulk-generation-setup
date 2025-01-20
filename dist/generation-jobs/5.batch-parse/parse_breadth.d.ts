f215e485da214d7f0855ec69f9a2ac1cbe3ea212
Shreedhar <shreedharpandey000@gmail.com>
2025-01-20 15:07:40 +0545

    Removed dist ffolder

--- dist/generation-jobs/5.batch-parse/parse_breadth.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- *
- * For parsing the breadth or typology
- *
- * @param {RawResponse} rawResponse from batch response
- * @returns {ParsedResponse}
- */
-export declare function parseBreadth(rawResponse: RawResponse): ParsedResponse;
--- dist/generation-jobs/5.batch-parse/parse_depth.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function parseDepth(rawResponse: RawResponse): ParsedResponse;
